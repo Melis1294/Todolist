c2d9467a616b19b78cd58fccdcbac78162acfe4d
klajdi.myftari <S267946@studenti.polito.it>
2022-01-25 18:08:38 +0100

    Update

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodolistAPI.Models
+{
+
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite("Data Source=/Users/klajdi.myftari/Projects/TodolistAPI/DBs/MySqliteDb_1.db");
+
+        public DbSet<TaskDetail> Tasks { get; set; }
+    }
+}
